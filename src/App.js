a423425d0e42899566bd56b3aae6342bb5e11b90
Gunveer Singh <72687410+Gun777veer@users.noreply.github.com>
2022-10-18 02:04:03 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Pathfindingvisualizer from './Pathfindingvisualizer/Pathfindingvisualizer'
+
+function App() {
+  return (
+
+    <div className="App">
+      <Pathfindingvisualizer></Pathfindingvisualizer>
+    </div>
+  );
+}
+
+export default App;
